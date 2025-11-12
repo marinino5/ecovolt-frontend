c761d88ef74794350cd9aa2539bbcd2f8c5a19b6
marinino5 <110548963+marinino5@users.noreply.github.com>
2025-11-12 18:21:51 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,12 @@
-// URL pública del backend (Ngrok)
-window.ECOVOLT_API = 'https://lanny-unintensified-guadalupe.ngrok-free.dev';
+// config.js
+
+// Base del backend (SIN el /health al final)
+const BACKEND_HTTP = "https://lanny-unintensified-guadalupe.ngrok-free.dev";
+
+// De momento no usamos WebSocket, así que lo dejamos vacío
+const BACKEND_WS = null;
+
+window.ECOVOLT_CONFIG = {
+  BACKEND_HTTP,
+  BACKEND_WS,
+};
