ec5e05a39246a3e76c8b11b701abbad5dae913a7
marinino5 <110548963+marinino5@users.noreply.github.com>
2025-11-12 18:28:33 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,6 @@
 // config.js
-
-// Base del backend (SIN el /health al final)
 const BACKEND_HTTP = "https://lanny-unintensified-guadalupe.ngrok-free.dev";
-
-// De momento no usamos WebSocket, así que lo dejamos vacío
-const BACKEND_WS = null;
+const BACKEND_WS   = null;
 
 window.ECOVOLT_CONFIG = {
   BACKEND_HTTP,
